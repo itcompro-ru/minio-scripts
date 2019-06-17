564a95da71a11fdb75c4be90bec93f6911ae0347
Vadim V <6600gm@gmail.com>
2019-06-17 18:58:15 +0300

    check user and policy

--- newuser.sh
+++ newuser.sh
@@ -6,9 +6,19 @@
 # MinIO client (mc) is required https://github.com/minio/mc
 ###
 
+die()
+{
+	local _ret=$2
+	test -n "$_ret" || _ret=1
+	test "$_PRINT_HELP" = yes && print_help >&2
+	echo "$1" >&2
+	exit ${_ret}
+}
+
+
+
 if ! [ -x "$(command -v mc)" ]; then
-  echo 'Error: mc (MinIO client) is not installed' >&2
-  exit 1
+  die 'Error: mc (MinIO client) is not installed (https://github.com/minio/mc)' 
 fi
 
 SERVER=$1
@@ -17,37 +27,39 @@ POLICY=$USER
 USAGE="Usage: $0 server user"
 if [[ -z $USER ]]; then
   echo "USER is empty"
-  echo $USAGE
-  exit
+  die $USAGE
 fi
 if [[ -z $SERVER ]]; then
   echo "SERVER is empty"
-  echo $USAGE
+  die $USAGE
   exit
 fi
 
-# TODO: check existing user
-# TODO: check existing policy
+if [[ $(mc admin user list minio --json | grep $USER | wc -l) != "0" ]]; then
+  die "error, user or poilicy allready exists"
+fi
 
 PASSWORD=$(head  /dev/urandom | sha1sum | egrep "\w+"  -o)
-BUCKET=$(echo "$USER" | sha1sum | egrep "\w+"  -o)
+BUCKET=$(head  /dev/urandom | sha1sum | egrep "\w+" -o)
+BUCKET="$USER${BUCKET:0:6}"
 TMPFILE="/tmp/$PASSWORD.json"
-echo "{\"Version\":\"$(date -I)\",\"Statement\":[{\"Effect\":\"Allow\",\"Action\":[\"s3:*\"],\"Resource\":[\"arn:aws:s3:::$BUCKET\"]}]}" > $TMPFILE
+
+echo "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Action\":[\"s3:*\"],\"Resource\":[\"arn:aws:s3:::$BUCKET\"]}]}" > $TMPFILE
 echo "Create bucket $BUCKET"
-mc mb $SERVER/$BUCKET
-if [[ $? -ne 0 ]]; then
-  exit
-fi
+
+
+
+mc mb $SERVER/$BUCKET || die "error"
+
 echo "Create policy $USER"
-mc admin policy add $SERVER $POLICY $TMPFILE
-if [[ $? -ne 0 ]]; then
-  exit
-fi
+mc admin policy add $SERVER $POLICY $TMPFILE || die "error"
+
 echo "Create user $USER"
-mc admin user add minio $USER $PASSWORD $POLICY
-if [[ $? -ne 0 ]]; then
-  exit
-fi
+mc admin user add $SERVER $USER $PASSWORD $POLICY || die "error"
+
+
+rm $TMPFILE
+
 echo "Access key: $USER"
 echo "Secret key: $PASSWORD"
 echo "Bucket: $BUCKET"
